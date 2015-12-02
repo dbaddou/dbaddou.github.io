5e7367a88fb95c65826a12f6be6e724bbe765a6e
unknown <dbaddou>
2015-12-02 11:21:27 -0500

    adding my files to my repository

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//monTitre.textContent = 'Encore Bonjour tout le monde!!';
+var monBouton = document.querySelector('button');
+//var monTitre = document.querySelector('h1');
+var monTitre = document.querySelector('h1');
+
+function definirNomUtilisateur() {
+  var monNom = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', monNom);
+  monTitre.textContent = 'Bonjour, ' + monNom;
+
+if(!localStorage.getItem('nom')) {
+  definirNomUtilisateur();
+} else {
+  var nomEnregistre = localStorage.getItem('nom');
+  monTitre.textContent = 'Bonjour, ' + nomEnregistre;
+}
+}
+
+monBouton.onclick = function() {
+  definirNomUtilisateur();
+}
+
+
+
+var monImage = document.querySelector('img');
+monImage.onclick = function() {
+    var maSrc = monImage.getAttribute('src');
+    if(maSrc === 'images/Desert.jpg') {
+      monImage.setAttribute ('src','images/Penguins.jpg');
+    } else {
+      monImage.setAttribute ('src','images/Desert.jpg');
+    }
+}
+
+// /////////////////////////////////
+
