3b13c426adccbbba44df958dd1dad888e387207e
dbaddou <dbaddou@yahoo.com>
2015-12-03 10:03:12 -0500

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,13 @@
+<html> 
+<head> 
+<title>
+<?php echo " Bonjour tout le monde!"; ?>
+</title> 
+</head> 
+<body>
+<h1>
+<?php echo “Bonjour tout le monde!";?>
+</h1>
+<p>
+<?php echo "Un bonjour."; ?>
+</p></body> </html>
